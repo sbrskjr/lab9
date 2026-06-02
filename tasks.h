e18a8d60e86478873079bab9d82007f4760ff289
sbrskjr <59169491+sbrskjr@users.noreply.github.com>
2026-06-02 14:47:23 +0300

    Create tasks.h

---
+++ tasks.h
@@ -0,0 +1,14 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void task1();
+void task2();
+void my_insert(char* str, int pos, const char* s);
+bool is_valid(const std::string& str);
+void process_string(std::string& s, const std::string& s1, const std::string& s2);
+
+#endif // TASKS_H
